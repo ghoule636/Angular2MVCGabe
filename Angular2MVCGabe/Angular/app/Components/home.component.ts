76a5832aac4416928ea617dd31695bc20a64e3bd
ghoule636 <ghoule636@gmail.com>
2019-02-05 20:23:59 -0800

    Yay page loads and everything works without an error so far

---
+++ Angular2MVCGabe/Angular/app/Components/home.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    template: `<div>Hello world!</div>`//<img src="../../../images/users.jpg" style="text-align:center"/>`
+})
+export class HomeComponent { }
---
+++ Angular2MVCGabe/Angular/app/app.module.ts
@@ -0,0 +1,18 @@
+﻿import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { HomeComponent } from './Components/home.component';
+
+let INITIAL_COMPONENTS = [AppComponent, HomeComponent]
+
+@NgModule({
+    imports: [BrowserModule, ReactiveFormsModule, HttpModule, routing],
+    declarations: [INITIAL_COMPONENTS],
+    providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }

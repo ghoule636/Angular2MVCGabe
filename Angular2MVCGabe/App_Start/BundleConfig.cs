aead24fe29a0b1344fb0781e21c7e1c1dfd0fa63
gabriel.houle <gabriel.houle@navy.mil>
2019-02-28 15:37:48 -0800

    working with styles to make a pretty page also we are now correctly loading fontawesome

--- Angular2MVCGabe/App_Start/BundleConfig.cs
+++ Angular2MVCGabe/App_Start/BundleConfig.cs
@@ -25,6 +25,11 @@ namespace Angular2MVCGabe
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/fontawesome").Include(
+                "~/Content/font-awesome.css"
+                ));
+
         }
     }
 }

596b58a8f406cf1c2efe6bb40c609675c6bffea1
ghoule636 <ghoule636@gmail.com>
2019-02-27 16:24:34 -0800

    adding log4net

---
+++ Angular2MVCGabe/Installers/LoggerInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Castle.Facilities.Startable;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Angular2MVCGabe.Installers
+{
+    public class LoggerInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            //container.AddFacility<LoggingFacility>(f => f.UseLog4Net());
+        }
+    }
+}
---
+++ Angular2MVCGabe/Installers/ODataInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Microsoft.AspNet.OData;
+
+namespace Angular2MVCGabe.Installers
+{
+    public class ODataInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly()
+                .BasedOn<ODataController>()
+                .WithService.DefaultInterfaces()
+                .LifestyleTransient());
+        }
+    }
+}
